2ef9361b2a805ed933b87f787571c5666c82884c
Caio Eduardo Ribeiro Beraldo Beraldo <caioeduber@hotmail.com>
2024-11-28 18:19:28 -0300

    progress: conexão banco

--- GestMarinha/paginaBuscar.Designer.cs
+++ GestMarinha/paginaBuscar.Designer.cs
@@ -33,6 +33,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -49,10 +51,11 @@
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(652, 220);
+            this.dataGridView2.Location = new System.Drawing.Point(648, 220);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.Size = new System.Drawing.Size(532, 372);
             this.dataGridView2.TabIndex = 5;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
             // 
             // label2
             // 
@@ -73,6 +76,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(307, 34);
             this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
@@ -82,6 +86,26 @@
             this.button1.TabIndex = 8;
             this.button1.Text = "Buscar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(790, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(307, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(790, 177);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(307, 34);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // paginaBuscar
             // 
@@ -89,6 +113,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::GestMarinha.Properties.Resources.Cadastrar_Animais;
             this.ClientSize = new System.Drawing.Size(1209, 604);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
@@ -110,5 +136,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- GestMarinha/paginaBuscar.cs
+++ GestMarinha/paginaBuscar.cs
@@ -9,7 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
-
 namespace GestMarinha
 {
     public partial class paginaBuscar : Form
@@ -19,20 +18,133 @@ namespace GestMarinha
             InitializeComponent();
         }
 
+        SqlConnection cn = new SqlConnection(@"Server=CAIOB_LC\SQLEXPRESS;Database=ControleMarinho;User Id=sa;Password=abacate03");
+        SqlCommand cm = new SqlCommand();
 
         private void label2_Click(object sender, EventArgs e)
         {
-
         }
 
         private void paginaBuscar_Load(object sender, EventArgs e)
         {
+            CarregarDadosDataGrid1();
+            CarregarDadosDataGrid2();
+        }
 
+        private void CarregarDadosDataGrid1()
+        {
+            try
+            {
+                string query1 = "SELECT * FROM dbo.Animais"; // Substitua com sua tabela
+                DataTable tabela1 = new DataTable();
+
+                cm.Connection = cn;
+                cm.CommandText = query1;
+
+                SqlDataAdapter da = new SqlDataAdapter(cm);
+                da.Fill(tabela1);
+
+                dataGridView1.DataSource = tabela1;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao buscar dados para DataGridView1: " + ex.Message);
+            }
+        }
+
+        private void CarregarDadosDataGrid2()
+        {
+            try
+            {
+                string query2 = "SELECT * FROM dbo.Tanques"; // Substitua com sua tabela
+                DataTable tabela2 = new DataTable();
+
+                cm.Connection = cn;
+                cm.CommandText = query2;
+
+                SqlDataAdapter da = new SqlDataAdapter(cm);
+                da.Fill(tabela2);
+
+                dataGridView2.DataSource = tabela2;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao buscar dados para DataGridView2: " + ex.Message);
+            }
+        }
+
+        private void PesquisarDataGrid1()
+        {
+            try
+            {
+                string termoPesquisa = textBox1.Text.Trim(); // Captura o texto da primeira TextBox
+                string query = "SELECT * FROM dbo.Animais WHERE Nome LIKE @pesquisa"; // Altere "Nome" para a coluna desejada
+                DataTable tabela = new DataTable();
+
+                cm.Connection = cn;
+                cm.CommandText = query;
+                cm.Parameters.Clear();
+                cm.Parameters.AddWithValue("@pesquisa", "%" + termoPesquisa + "%");
+
+                SqlDataAdapter da = new SqlDataAdapter(cm);
+                da.Fill(tabela);
+
+                dataGridView1.DataSource = tabela;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao buscar dados no DataGridView1: " + ex.Message);
+            }
+        }
+
+        private void PesquisarDataGrid2()
+        {
+            try
+            {
+                string termoPesquisa = textBox2.Text.Trim(); // Captura o texto da segunda TextBox
+                string query = "SELECT * FROM dbo.Tanques WHERE Descricao LIKE @pesquisa"; // Altere "Descricao" para a coluna desejada
+                DataTable tabela = new DataTable();
+
+                cm.Connection = cn;
+                cm.CommandText = query;
+                cm.Parameters.Clear();
+                cm.Parameters.AddWithValue("@pesquisa", "%" + termoPesquisa + "%");
+
+                SqlDataAdapter da = new SqlDataAdapter(cm);
+                da.Fill(tabela);
+
+                dataGridView2.DataSource = tabela;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao buscar dados no DataGridView2: " + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PesquisarDataGrid1(); // Executa a pesquisa no primeiro DataGridView
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PesquisarDataGrid2(); // Executa a pesquisa no segundo DataGridView
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
+        }
 
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
         }
     }
 }
